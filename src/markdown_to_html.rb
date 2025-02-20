5bf3940d252d56f0ec88ef9b5942c520b21ca7a3
anonymByte-404 <an0nymnx@outlook.com>
2025-02-19 19:00:42 -0500

    feat: Add format option to Markdown to HTML conversion

--- src/markdown_to_html.rb
+++ src/markdown_to_html.rb
@@ -1,5 +1,9 @@
 require 'kramdown'
 
-def convert_markdown_to_html(markdown_text)
-  Kramdown::Document.new(markdown_text).to_html
+def convert_markdown_to_html(markdown_text, format = 'html')
+  if format == 'html'
+    Kramdown::Document.new(markdown_text).to_html
+  else
+    return markdown_text
+  end
 end
